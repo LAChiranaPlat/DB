20e84f2247ad8fa7f93afd81032ffd2d452ca686
LAChiranaPlat <fullwingchun@gmail.com>
2023-04-16 10:34:22 -0500

    Inflanfo Layout MainActivity

--- app/src/main/java/com/example/db/MainActivity.kt
+++ app/src/main/java/com/example/db/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.db
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.db.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var layout:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        layout= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(layout.root)
     }
 }
